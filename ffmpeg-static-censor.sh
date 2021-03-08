bc415f46140c883851efee3c49ffc45f3b645678
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2021-03-08 14:37:35 +0200

    remove comment

--- ffmpeg-static-censor.sh
+++ ffmpeg-static-censor.sh
@@ -5,6 +5,4 @@ if [ "$#" -ne 3 ]; then
 	exit
 fi
 
-#ffmpeg -i snarpii-noo.mp4 -i mask.png -filter_complex "[0:v][1:v]alphamerge,boxblur=10[alf];[0:v][alf]overlay[v]" -map "[v]" -map 0:a -c:v libx264 -c:a copy -movflags +faststart snmasked.mp4
-
 ffmpeg -i "$1" -i "$2" -filter_complex "[0:v][1:v]alphamerge,boxblur=10[alf];[0:v][alf]overlay[v]" -map "[v]" -map 0:a -c:v libx264 -c:a copy -movflags +faststart "$3"
