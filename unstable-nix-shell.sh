996f41cd436559db590d5552eb7b8557dc3b1577
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2022-05-19 18:26:43 +0300

    bump nixpkgs

--- unstable-nix-shell.sh
+++ unstable-nix-shell.sh
@@ -4,7 +4,7 @@ nix-shell -E "
 	#{ nixpkgs ? import <nixpkgs> {} }:
 	let
 	  fromNixpkgsCommit = commit: fetchTarball (\"https://github.com/NixOS/nixpkgs/archive/\" + commit + \".tar.gz\");
-  	  unstablePkgs = import (fromNixpkgsCommit \"79dd82eadd34300b04d1363be6a2ab84670ac0cb\") {};
+  	  unstablePkgs = import (fromNixpkgsCommit \"52dc75a4fee3fdbcb792cb6fba009876b912bfe0\") {};
 	in
 	with unstablePkgs; mkShell {
 	  buildInputs = [ $* ];
